a7727af48b9a761540350151a58bb7eeb86a9262
Sparkle Party <lexi.slovik@gmail.com>
2021-12-20 21:57:48 -0800

    try to add files

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
+const profileDataArgs = process.argv.slice(2);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const [name, github] = profileDataArgs;
 
-}
+const pageHTML = generatePage(name, github);
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
